039bce37467da201db73237e469720c673c9334c
DESKTOP-U97DCG1\user <soobinnunu1101@gmail.com>
2024-08-27 17:43:14 +0900

    0827 commit

---
+++ app/api/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+
+    const response = {
+        message: '호호호',
+        data: '오늘도 코딩'
+    }
+
+    return NextResponse.json(response, { status: 200});
+}
